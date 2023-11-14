ce859af428d1b90fba4c689b5105ba756d4cb925
Roman <gremkil@gmail.com>
2023-11-14 20:57:49 +0300

    fea: build works

--- src/code/main.zig
+++ src/code/main.zig
@@ -1,7 +1,37 @@
 const std = @import("std");
+const rl = @import("raylib.zig");
+const ecs = @import("zig-ecs");
 
 pub fn main() void {
-    var a: i32 = 44;
-    a += 3;
-    std.debug.print("Hello World", .{});
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    rl.InitWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    defer rl.CloseWindow(); // Close window and OpenGL context
+
+    rl.SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    var reg = ecs.Registry.init(std.heap.c_allocator);
+    defer reg.deinit();
+
+    // Main game loop
+    while (!rl.WindowShouldClose()) { // Detect window close button or ESC key
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        rl.BeginDrawing();
+        defer rl.EndDrawing();
+
+        rl.ClearBackground(rl.WHITE);
+
+        rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.RED);
+        //----------------------------------------------------------------------------------
+    }
 }
